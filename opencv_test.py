d2fcb0418a76a2fc97733efaa1ba110756243fac
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-06-09 17:47:33 +0900

    Create opencv_test.py

---
+++ opencv_test.py
@@ -0,0 +1,16 @@
+import cv2
+
+# 이미지 파일 경로
+image_path = 'home\\ubuntu\\2024_EdgeComputing_Pothole\\test\\test_a.jpg'
+
+# 이미지 읽기
+image = cv2.imread(image_path)
+
+# 이미지가 있는지 확인
+if image is None:
+    print("이미지를 읽을 수 없습니다.")
+else:
+    # 이미지 출력
+    cv2.imshow('Image', image)
+    cv2.waitKey(0)  # 아무 키나 누를 때까지 대기
+    cv2.destroyAllWindows()  # 창 닫기
