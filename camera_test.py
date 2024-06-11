13b02ee69b182437b7e5d9cfaa3065fb408884c0
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-06-11 15:10:08 +0900

    Update camera_test.py

--- camera_test.py
+++ camera_test.py
@@ -1,3 +1,10 @@
+# MIT License
+# Copyright (c) 2019-2022 JetsonHacks
+
+# Using a CSI camera (such as the Raspberry Pi Version 2) connected to a
+# NVIDIA Jetson Nano Developer Kit using OpenCV
+# Drivers for the camera and OpenCV are included in the base image
+
 import cv2
 
 cap = cv2.VideoCapture(0)
