e0cedaa5e8eced118e3a43970893e68d714b2f7e
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-06-11 21:23:12 +0900

    Update

---
+++ yolov5/main.py
@@ -0,0 +1,26 @@
+import threading
+import pothole_module.Pothole_GPS
+import pothole_module.Pothole_Yolo
+
+from detect import run_yolov5
+
+
+class Pothole_detector:
+    def __init__(self):
+        self.GPS = pothole_module.Pothole_GPS.pothole_GPS()
+
+
+
+    def run(self):
+        yolov5_thread = threading.Thread(target=run_yolov5)
+        yolov5_thread.start()
+
+
+
+def main():
+    pth_detector = Pothole_detector()
+    pth_detector.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ yolov5/pothole_module/Pothole_GPS.py
@@ -0,0 +1,30 @@
+import serial
+import pynmea2
+
+
+class pothole_GPS:
+    def __init__(self):
+        self.latitude = 35.830615
+        self.logitude = 128.754465
+
+    def read_gps_data(self):
+        # 시리얼 포트 설정 (적절한 포트와 보드레이트로 설정)
+        port = '/dev/ttyTHS1'  # Jetson Nano의 UART 포트
+        baudrate = 9600  # 일반적으로 Neo-7M의 보드레이트는 9600
+
+        # 시리얼 포트 열기
+        ser = serial.Serial(port, baudrate, timeout=1)
+
+        try:
+            while True:
+                line = ser.readline().decode('ascii', errors='replace')
+                if line.startswith('$GNGGA') or line.startswith('$GPGGA'):
+                    msg = pynmea2.parse(line)
+                    self.latitude = msg.latitude
+                    self.logitude = msg.longitude
+        except Exception as e:
+            print("System::Can't get GPS pos")
+        ser.close()
+        return self.latitude, self.logitude
+
+
