91aa78be7d835a3e78e3dbbb90a8cba642a2c171
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-06-12 11:52:14 +0900

    Update

--- yolov5/main.py
+++ yolov5/main.py
@@ -1,17 +1,20 @@
 import threading
 import pothole_module.Pothole_GPS
-from pothole_module.Pothole_Yolo import run_yolov5
-
+import pothole_module.Pothole_Yolo
+import pothole_module.Pothole_information
 
 class Pothole_detector:
     def __init__(self):
+        self.info=pothole_module.Pothole_information.information()
         self.GPS = pothole_module.Pothole_GPS.pothole_GPS()
-
+        self.PotholeDetectorYOLO = pothole_module.Pothole_Yolo.PotholeDetector(info)
 
 
     def run(self):
-        yolov5_thread = threading.Thread(target=run_yolov5)
+        yolov5_thread = threading.Thread(target=self.PotholeDetectorYOLO.run)
         yolov5_thread.start()
+        while True:
+            print(self.info.detect)
 
 
 
---
+++ yolov5/pothole_module/Pothole_information.py
@@ -0,0 +1,4 @@
+class information:
+    def __init__(self):
+        self.detect = False
+        
