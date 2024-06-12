1eca803ac2d955504f5a15f449ccd211137260c2
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-06-12 11:29:53 +0900

    Update Pothole_Yolo.py

--- yolov5/pothole_module/Pothole_Yolo.py
+++ yolov5/pothole_module/Pothole_Yolo.py
@@ -38,7 +38,7 @@ from utils.torch_utils import select_device, smart_inference_mode
 
 @smart_inference_mode()
 def run(
-    weights=ROOT / "yolov5s.pt",  # model path or triton URL
+    weights=ROOT / "best.pt",  # model path or triton URL
     source=ROOT / "data/images",  # file/dir/URL/glob/screen/0(webcam)
     data=ROOT / "data/coco128.yaml",  # dataset.yaml path
     imgsz=(640, 640),  # inference size (height, width)
@@ -231,7 +231,7 @@ def run(
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--weights", nargs="+", type=str, default=str("/home/ubuntu/2024_EdgeComputing_Pothole/yolov5/best.pt"), help="model path or triton URL")
+    parser.add_argument("--weights", nargs="+", type=str, default="best.pt", help="model path or triton URL")
     parser.add_argument("--source", type=str, default="0", help="file/dir/URL/glob/screen/0(webcam)")
     parser.add_argument("--data", type=str, default=ROOT / "data/coco128.yaml", help="(optional) dataset.yaml path")
     parser.add_argument("--imgsz", "--img", "--img-size", nargs="+", type=int, default=[640], help="inference size h,w")
