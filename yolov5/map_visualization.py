2d84cb9f34049d0b0eca98e5bc49de0b480e661c
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-06-19 15:27:52 +0900

    Update

--- yolov5/map_visualization.py
+++ yolov5/map_visualization.py
@@ -7,7 +7,7 @@ import threading
 class pothole_visualization:
     def __init__(self):
         # 지도 생성 (위치: 서울, 줌 레벨: 12)
-        self.map = folium.Map(location=[35.830615, 128.754465], zoom_start=12)  # 영남대학교 IT 관 기준
+        self.map = folium.Map(location=[35.830615, 128.754465], zoom_start=14)  # 영남대학교 IT 관 기준
         # UDP 서버 설정
         self.UDP_IP = "165.229.185.185"  # 수신 IP 주소
         self.UDP_PORT = 8080  # 수신 포트 번호
@@ -49,13 +49,37 @@ class pothole_visualization:
         while True:
             data, addr = self.sock.recvfrom(65536)
             message = data.decode("utf-8")
-            print(f"Received message: {message}")
+            first_comma_idx = message.find(',')
+            if first_comma_idx != -1:
+                latitude = message[:first_comma_idx]
+                remaining_message = message[first_comma_idx + 1:]
 
-            # 수신된 메시지 형식: "latitude,longitude,popup,image_path"
-            latitude, longitude, popup, recv_img = message.split(',')
+                # 두번째 쉼표 위치 찾기
+                second_comma_idx = remaining_message.find(',')
+                if second_comma_idx != -1:
+                    longitude = remaining_message[:second_comma_idx]
+                    remaining_message = remaining_message[second_comma_idx + 1:]
+
+                    # 세번째 쉼표 위치 찾기
+                    third_comma_idx = remaining_message.find(',')
+                    if third_comma_idx != -1:
+                        popup = remaining_message[:third_comma_idx]
+                        recv_img = remaining_message[third_comma_idx + 1:]
+                    else:
+                        # 예외 처리: 세번째 쉼표를 찾을 수 없는 경우
+                        print("Error: Third comma not found in message.")
+                        latitude, longitude, popup, recv_img = None, None, None, None
+                else:
+                    # 예외 처리: 두번째 쉼표를 찾을 수 없는 경우
+                    print("Error: Second comma not found in message.")
+                    latitude, longitude, popup, recv_img = None, None, None, None
+            else:
+                # 예외 처리: 첫번째 쉼표를 찾을 수 없는 경우
+                print("Error: First comma not found in message.")
+                latitude, longitude, popup, recv_img = None, None, None, None
             location = [float(latitude), float(longitude)]
             image_filename = f"received_image_{popup}.jpg"  # 예시로 jpg 확장자 사용
-            image_path = os.path.join("/img", image_filename)  # 저장할 디렉토리 경로 설정
+            image_path = os.path.join("\\img", image_filename)  # 저장할 디렉토리 경로 설정
 
             with open(image_path, 'wb') as f:
                 f.write(recv_img)  # recv_img를 파일에 쓰기
